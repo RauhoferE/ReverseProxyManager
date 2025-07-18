e9bc620c366aadb6afb9e2cd610f647adcaf2683
Emre Rauhofer <emre.rauhofer@gmail.com>
2025-07-18 11:59:33 +0200

    Added validator for login

---
+++ ReverseProxyManager/ReverseProxyManager/Requests/LoginRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ReverseProxyManager.Requests
+{
+    public class LoginRequest
+    {
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ ReverseProxyManager/ReverseProxyManager/Validation/LoginRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using ReverseProxyManager.Requests;
+
+
+namespace ReverseProxyManager.Validation
+{
+    public class LoginRequestValidator: AbstractValidator<LoginRequest>
+    {
+        public LoginRequestValidator()
+        {
+            this.RuleFor(x => x.Name).NotEmpty().NotNull();
+            this.RuleFor(x => x.Password).NotEmpty().NotNull();
+        }
+    }
+}
