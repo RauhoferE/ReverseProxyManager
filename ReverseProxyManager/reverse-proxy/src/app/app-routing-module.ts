84d50bbd281de0a0a665fee8a6c6a792a64d9c62
Emre Rauhofer <emre.rauhofer@gmail.com>
2025-08-12 15:48:45 +0200

    Added services a guard and added package for cookies

--- ReverseProxyManager/reverse-proxy/src/app/app-routing-module.ts
+++ ReverseProxyManager/reverse-proxy/src/app/app-routing-module.ts
@@ -5,6 +5,7 @@ import { Login } from './views/login/login';
 import { ServerManager } from './views/server-manager/server-manager';
 import { CertManager } from './views/cert-manager/cert-manager';
 import { Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth-guard';
 
 const routes: Routes = [
   // This is the default route that loads the HomeComponent
@@ -12,11 +13,13 @@ const routes: Routes = [
     children:[
       {
       path:'server',
-      component: ServerManager
+      component: ServerManager,
+      canActivate:[AuthGuard]
       },
       {
         path:'certs',
-        component: CertManager
+        component: CertManager,
+        canActivate:[AuthGuard]
       }
 
     ]
