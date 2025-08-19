84b61e12cb7ffc8d32ee4a9b04ad044736c2a9b0
Emre <emre.rauhofer@gmail.com>
2025-08-19 06:36:23 +0200

    Changed angular project from version 20 to 18 and added ng-zorro

---
+++ ReverseProxyManager/reverse-proxy/src/app/modals/cert-edit/cert-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cert-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './cert-edit.component.html',
+  styleUrl: './cert-edit.component.scss'
+})
+export class CertEditComponent {
+
+}
---
+++ ReverseProxyManager/reverse-proxy/src/app/modals/server-edit/server-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './server-edit.component.html',
+  styleUrl: './server-edit.component.scss'
+})
+export class ServerEditComponent {
+
+}
