1f65392ff3e04029a8d5e191c60dd893675b3c43
Emre <emre.rauhofer@gmail.com>
2025-08-19 18:07:41 +0200

    Worked on certificates view

--- ReverseProxyManager/reverse-proxy/src/app/views/certificates/certificates.component.ts
+++ ReverseProxyManager/reverse-proxy/src/app/views/certificates/certificates.component.ts
@@ -1,25 +1,28 @@
 import { Component } from '@angular/core';
-import { NzTableModule, NzTableSortOrder } from 'ng-zorro-antd/table';
+import { NzTableModule, NzTableQueryParams, NzTableSortOrder } from 'ng-zorro-antd/table';
 import { CertificateDto } from '../../models/certificateModels';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconDirective, NzIconModule, provideNzIcons } from 'ng-zorro-antd/icon';
+import { CheckOutline } from '@ant-design/icons-angular/icons';
+import { CommonModule } from '@angular/common';
+import dayjs from 'dayjs';
+import { NzFlexModule } from 'ng-zorro-antd/flex';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
 
 @Component({
   selector: 'app-certificates',
   standalone: true,
-  imports: [NzTableModule, NzButtonModule],
+  imports: [NzTableModule, NzButtonModule, NzIconModule, CommonModule, NzFlexModule, NzSpaceModule],
+  // providers:[provideNzIcons(CheckOutline)],
   templateUrl: './certificates.component.html',
   styleUrl: './certificates.component.scss'
 })
 export class CertificatesComponent {
+
+
+
   certificates: CertificateDto[] = [];
-  sortDirections: any[] = ['ascend', 'descend', null];
-  column: any;
-  name_sortOrder: NzTableSortOrder = null;
-  issuer_sortOrder: NzTableSortOrder = null;
-  validNotBefore_sortOrder: NzTableSortOrder = null;
-  validNotAfter_sortOrder: NzTableSortOrder = null;
-  lastUpdated_sortOrder: NzTableSortOrder = null;
-  fileAttached_sortOrder: NzTableSortOrder = null;
+  loading: boolean = false;
 
   /**
    *
@@ -28,5 +31,20 @@ export class CertificatesComponent {
     
     
   }
+
+  onQueryParamsChange($event: NzTableQueryParams) {
+    const { pageSize, pageIndex, sort, filter } = $event;
+    const currentSort = sort.find(item => item.value !== null);
+    const sortField = (currentSort && currentSort.key) || null;
+    const sortOrder = (currentSort && currentSort.value) || null;
+}
+
+  rescanCertificates() {
+  throw new Error('Method not implemented.');
+  }
+
+  getDate(arg0: Date) {
+    dayjs(arg0).format('YYYY-MM-DD HH:mm:ss');
+}
 
 }
