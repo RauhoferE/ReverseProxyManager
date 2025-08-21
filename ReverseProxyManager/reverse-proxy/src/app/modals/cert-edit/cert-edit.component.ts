b430b3ec74a9e693471363f6966b3fc7fe51f3f1
Emre <emre.rauhofer@gmail.com>
2025-08-21 12:04:54 +0200

    Added certificate modal

--- ReverseProxyManager/reverse-proxy/src/app/modals/cert-edit/cert-edit.component.ts
+++ ReverseProxyManager/reverse-proxy/src/app/modals/cert-edit/cert-edit.component.ts
@@ -1,30 +1,51 @@
-import { Component } from '@angular/core';
-import { NzModalModule } from 'ng-zorro-antd/modal';
+import { Component, EventEmitter, Inject, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
+import { bootstrapCheckCircle, bootstrapKey } from '@ng-icons/bootstrap-icons';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NZ_MODAL_DATA, NzModalModule, NzModalRef } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'app-cert-edit',
   standalone: true,
-  imports: [NzModalModule],
+  imports: [NzModalModule, FormsModule, NzFormModule, NzInputModule, ReactiveFormsModule, NzButtonModule, 
+    NgIcon
+  ],
+  providers:[provideIcons({bootstrapKey })],
   templateUrl: './cert-edit.component.html',
   styleUrl: './cert-edit.component.scss'
 })
-export class CertEditComponent {
+export class CertEditComponent implements OnInit {
 
-isVisible: boolean = true;
+certForm: FormGroup = {} as any;
 
 /**
  *
  */
-constructor() {
-  
+constructor(private fb: FormBuilder, private modal: NzModalRef, @Inject(NZ_MODAL_DATA) public data: any) {
+      this.certForm = this.fb.group({
+        name: [data['certName'], [
+          Validators.required,
+        ]]
+      });
   
 }
 
-handleOk() {
-throw new Error('Method not implemented.');
-}
+  ngOnInit(): void {
+  }
+
 handleCancel() {
-throw new Error('Method not implemented.');
+  this.modal.destroy(null);
 }
+
+submit() {
+  this.modal.destroy(this.name?.value);
+}
+
+get name() {
+    return this.certForm.get('name');
+  }
 
 }
--- ReverseProxyManager/reverse-proxy/src/app/views/certificates/certificates.component.ts
+++ ReverseProxyManager/reverse-proxy/src/app/views/certificates/certificates.component.ts
@@ -7,18 +7,18 @@ import { CommonModule } from '@angular/common';
 import dayjs from 'dayjs';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import {bootstrapX, bootstrapCheckCircle, bootstrapBoxArrowDown  } from '@ng-icons/bootstrap-icons';
+import { CertEditComponent } from "../../modals/cert-edit/cert-edit.component";
+import { NzModalModule, NzModalService } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'app-certificates',
   standalone: true,
-  imports: [NzTableModule, NzButtonModule, NzIconModule, CommonModule, NgIcon],
+  imports: [NzTableModule, NzButtonModule, NzIconModule, CommonModule, NgIcon, CertEditComponent, NzModalModule],
   providers:[provideIcons({bootstrapX, bootstrapCheckCircle, bootstrapBoxArrowDown })],
   templateUrl: './certificates.component.html',
   styleUrl: './certificates.component.scss'
 })
 export class CertificatesComponent {
-
-
 
   certificates: CertificateDto[] = [];
   loading: boolean = false;
@@ -26,7 +26,7 @@ export class CertificatesComponent {
   /**
    *
    */
-  constructor() {
+  constructor(private modalService: NzModalService) {
     
     
   }
@@ -39,7 +39,24 @@ export class CertificatesComponent {
 }
 
   rescanCertificates() {
-  throw new Error('Method not implemented.');
+   throw new Error("Method not implemented.");  
+
+  }
+
+  openEditCertModal(id: number, certName: string) {
+   var modalRef = this.modalService.create({
+    nzTitle: 'Edit Certificate',    
+      nzContent: CertEditComponent,
+    nzData: { certName: certName },
+    nzFooter: null,});
+
+    modalRef.afterClose.subscribe((result: string) => {
+      console.log("Modal closed with result: ", result);
+    });
+  }
+
+  updateCertName($event: string) {
+    console.log("Certificate name updated to: ", $event);
   }
 
   getDate(arg0: Date) {
